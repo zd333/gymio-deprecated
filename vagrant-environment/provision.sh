8d965734f13e746c8abe7fe0dff8e13962103a66
Igor Moroz <imoroz@localhost>
2015-09-24 13:39:42 +0300

    add jenkins job to initial mysql DB

--- vagrant-environment/provision.sh
+++ vagrant-environment/provision.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+CONF_DIR=//vagrant/vagrant-environment/conf
 QUIET_APT=-qq
 
 wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
@@ -8,8 +9,11 @@ sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again p
 
 # Set things up
 sudo apt-get update --yes $QUIET_APT
-
-sudo apt-get install dos2unix -y $QUIET_APT
-
 # Java 1.7.0_51 is what Amazon has...
-sudo apt-get install --reinstall  -y --force-yes -o Dpkg::options::=--force-confold $QUIET_APT openjdk-7-jre-headless=7u51-2.4.6-1ubuntu4 openjdk-7-jre=7u51-2.4.6-1ubuntu4 openjdk-7-jdk=7u51-2.4.6-1ubuntu4 jenkins mysql-server
+sudo apt-get install --reinstall  -y --force-yes -o Dpkg::options::=--force-confold $QUIET_APT openjdk-7-jre-headless=7u51-2.4.6-1ubuntu4 openjdk-7-jre=7u51-2.4.6-1ubuntu4 openjdk-7-jdk=7u51-2.4.6-1ubuntu4 jenkins mysql-server mc dos2unix
+
+echo "Copying Jenkins job config files ..."
+sudo cp -r $CONF_DIR/jenkins/* /var/lib/jenkins/jobs/
+sudo chown -R vagrant:vagrant /var/lib/jenkins/jobs/
+sudo chmod -R 777  /var/lib/jenkins/jobs/
+sudo service jenkins restart || exit 1
